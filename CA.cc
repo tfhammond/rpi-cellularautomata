5bd77f6230c7ca9813d152d8838c64004b54044e
Thomas <tommyfhammond@gmail.com>
2023-11-22 21:06:37 -0800

    Update CA.cc

--- CA.cc
+++ CA.cc
@@ -39,7 +39,7 @@ static void Rule30(Canvas *canvas){
 
     // Update the 'state' using the Rule 30 logic: (left, current, right) -> left XOR (current OR right)
     state = (state >> 1) ^ (state | state << 1);
-    usleep(1 * 1000);
+    usleep(1 * 1000000);
   }
 
 
